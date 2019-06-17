134afa7bc4ec4ec2c92f12da1c69ce8ac458e8d4
stephenmcfarland <snmcfarland@gmail.com>
2019-06-17 19:59:59 -0400

    filtering

--- Contoso/Pages/Students/Index.cshtml.cs
+++ Contoso/Pages/Students/Index.cshtml.cs
@@ -29,13 +29,20 @@ namespace Contoso.Pages.Students
         //    Student = await _context.Student.ToListAsync();
         //}
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            CurrentFilter = searchString;
 
             IQueryable<Student> studentIQ = from s in _context.Student
                                             select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentIQ = studentIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
 
             switch (sortOrder)
             {
